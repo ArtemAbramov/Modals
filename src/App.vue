0032a74b488833ba302d9a3f80c2d2775fbce89d
Artem Abramov <tanukisuicide@yandex.ru>
2019-05-05 13:59:44 +0300

    Init project, create Modal component, add close on click ""

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="wrapper">
+    <div class='wrapper-content'>
+      <section>
+        <div class="container">
+          <button class="btn btnPrimary" @click='modalFirst = !modalFirst'>Show first modal</button>
+          <!-- first modal -->
+          <modals 
+            title='First modal'
+            v-if='modalFirst'
+            @close='modalFirst = !modalFirst'>
+          </modals>
+        </div>
+      </section>      
+    </div>
+  </div>
+</template>
+
+<script>
+import modals from '@/components/Modal.vue'
+export default {
+  components: { modals },
+  data () {
+    return {
+      modalFirst: false
+    }
+  }
+}
+</script>
